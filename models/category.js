a2f654308d5beefdd9d655d1339d6716e38d062c
yanyanyaa <yclin3411@gmail.com>
2023-07-23 23:04:44 +0800

    feat: add registration feature and page

--- models/category.js
+++ models/category.js
@@ -2,10 +2,6 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const categorySchema = new Schema({
-  id: {
-    type: Number,
-    required: true
-  },
   name: {
     type: String,
     required: true
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,4 +1,52 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../../models/user.js')
+
+// register
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
+// data: register
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  User.findOne({ email })
+    .then(user => {
+      if (user) {
+        console.log('This user already exists.')
+        res.render('register', {
+          name,
+          email,
+          password,
+          confirmPassword
+        })
+      } else {
+        return User.create({
+          name,
+          email,
+          password
+        })
+        .then(() => res.redirect('/'))
+        .catch(err => console.log(err))
+      }
+    })
+    .catch(err => console.log(err))
+})
+
+// login
+router.get('/login', (req, res) => {
+  res.render('login')
+})
+
+// data: login
+router.post('/login', (req, res) => {
+
+})
+
+// data: logout
+router.get('/logout', (req, res) => {
+
+})
+
 
 module.exports = router
