3567e8a3b5d8f1dd9af4927f71c9510194e98b94
yanyanyaa <yclin3411@gmail.com>
2023-07-23 11:39:10 +0800

    feat: add edit routes and partials

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ if (process.env.NODE_ENV !== 'production') {
 
 require('./config/mongoose')
 
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs'}))
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(express.urlencoded({ extended: true }))
@@ -22,10 +22,10 @@ app.use(methodOverride('_method'))
 // index
 app.get('/', (req, res) => {
   Record.find()
-  .lean()
-  .sort({ _id: 'asc'})
-  .then(records => res.render('index', { records }))
-  .catch(err => console.log('err'))
+    .lean()
+    .sort({ _id: 'asc' })
+    .then(records => res.render('index', { records }))
+    .catch(err => console.log('err'))
 })
 
 // new
@@ -37,17 +37,44 @@ app.get('/records/new', (req, res) => {
 })
 
 // data: new
-app.post('/records', (req, res) => {  
+app.post('/records', (req, res) => {
   console.log(req.body)
-  const { name, date, category, amount} = req.body
-  return Record.create({ name, date, category, amount })  
+  const { name, date, category, amount } = req.body
+  return Record.create({ name, date, category, amount })
     .then(() => res.redirect('/'))
     .catch(err => console.log(err))
 })
 
 // edit
+app.get('/records/:id/edit', (req, res) => {
+  const _id = req.params.id
+  return Record.findById(_id)
+    .lean()
+    .then(record => {
+      return Catrgory.find()
+        .lean()
+        .sort({ _id: 'asc' })
+        .then(categories => 
+          res.render('edit', { record, categories }))
+    })
+})
 
 // data: edit
+app.put('/records/:id', (req, res) => {
+  const _id = req.params.id
+  const { name, date, amount, category } = req.body
+  return Record.findById(_id)
+    .then(record => {
+      record.name = name
+      record.date = date
+      record.amount = amount
+      record.category = category
+      return record.save()
+    })
+    .then(() => res.redirect('/'))
+    .catch(err => console.log(err))
+})
+
 
 // data: delete
 
--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,13 +1,17 @@
-const mongoose = require('mongoose')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+const db = require('../../config/mongoose.js')
+
 const Record = require('../record.js')
 const Category = require('../category.js')
 const User = require('../user.js')
-const db = mongoose.connection
 
 db.once('open', () => {
   Record.create({
-    name: 早餐,
+    name: '早餐',
     date: 2023-01-02,
-    amount: 100
+    amount: 100, 
+    Category: '家居物業'
   })
 })
